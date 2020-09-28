84f3eb2a7174a433a9da6277b58b318c2702ee99
TheMaybeast <themaybeast@gmail.com>
2020-09-28 04:03:16 -0300

    Update AssemblyInfo.cs

--- DLS/Properties/AssemblyInfo.cs
+++ DLS/Properties/AssemblyInfo.cs
@@ -14,5 +14,5 @@ using System.Runtime.InteropServices;
 
 [assembly: Guid("283d9119-0bbe-42a0-8f57-49e580f20584")]
 
-[assembly: AssemblyVersion("1.3.2.0")]
-[assembly: AssemblyFileVersion("1.3.2.0")]
+[assembly: AssemblyVersion("1.4.0.0")]
+[assembly: AssemblyFileVersion("1.4.0.0")]
